bceaa644bcb0ff47339f7fd81d5604416aea521e
王俊仁 <jr-wong@qq.com>
2016-10-10 15:22:22 +0800

    Initial Commit

---
+++ JRBaseRequest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JRBaseRequest
+//
+//  Created by J on 2016/10/10.
+//  Copyright © 2016年 J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JRBaseRequest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JRBaseRequest
+//
+//  Created by J on 2016/10/10.
+//  Copyright © 2016年 J. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
